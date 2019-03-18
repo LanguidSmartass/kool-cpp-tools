a31303999b45112a8bc16728fc6184d8cad1cdac
LanguidSmartass <jedi.orden@gmail.com>
2019-03-18 03:21:20 +0300

    update cmake file list template variable

--- install/files-list.cmake
+++ install/files-list.cmake
@@ -1,11 +1,18 @@
 set(
     headers-list-template
     
-    PREFIX_DIR/allocators/fat_and_fast.hpp
+    PREFIX_DIR/allocators/beefy.hpp
+    
+    PREFIX_DIR/prints/fmt.hpp
+    PREFIX_DIR/prints/print.hpp
+    PREFIX_DIR/prints/printf_light.hpp
 
+    PREFIX_DIR/bitwise.hpp
     PREFIX_DIR/c_array.hpp
     PREFIX_DIR/debug.hpp
     PREFIX_DIR/endian.hpp
+    PREFIX_DIR/log2.hpp
+    PREFIX_DIR/pow2.hpp
     PREFIX_DIR/range.hpp
     PREFIX_DIR/sequence.hpp
 )
