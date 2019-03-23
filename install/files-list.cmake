2cc58a699d1946b7663f71797f27108a0673181e
LanguidSmartass <jedi.orden@gmail.com>
2019-03-23 11:53:35 +0300

    update cmake file list template

--- install/files-list.cmake
+++ install/files-list.cmake
@@ -2,12 +2,17 @@ set(
     headers-list-template
     
     PREFIX_DIR/allocators/beefy.hpp
+
+    PREFIX_DIR/io/common/builtin.hpp
+    PREFIX_DIR/io/common/conversion.hpp
+    PREFIX_DIR/io/common/conversion_table.hpp
+    PREFIX_DIR/io/common/fmt.hpp
     
-    PREFIX_DIR/prints/fmt.hpp
-    PREFIX_DIR/prints/print.hpp
-    PREFIX_DIR/prints/printf_light.hpp
+    PREFIX_DIR/io/printf/str_and_int.hpp
+    PREFIX_DIR/io/out_str_and_int.hpp
 
     PREFIX_DIR/bitwise.hpp
+    PREFIX_DIR/byte.hpp
     PREFIX_DIR/c_array.hpp
     PREFIX_DIR/debug.hpp
     PREFIX_DIR/endian.hpp
@@ -15,4 +20,6 @@ set(
     PREFIX_DIR/pow2.hpp
     PREFIX_DIR/range.hpp
     PREFIX_DIR/sequence.hpp
+    PREFIX_DIR/traits.hpp
+    PREFIX_DIR/util.hpp
 )
