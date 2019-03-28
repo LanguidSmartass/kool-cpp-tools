60a2093d58dfcc8ca0e67a1bf34f2f835dccc85d
LanguidSmartass <jedi.orden@gmail.com>
2019-03-28 23:48:27 +0300

    add support for 'byte' type

--- install/headers/traits.hpp
+++ install/headers/traits.hpp
@@ -33,28 +33,71 @@
 #ifndef KCPPT_TRAITS_HPP
 #define KCPPT_TRAITS_HPP
 
-#include <type_traits>
+#include "byte.hpp"
 
 namespace kcppt {
 
 namespace traits {
 
 template <typename V>
-constexpr static auto is_class_v = std::is_class_v<V>;
+constexpr static auto is_unsigned_byte_v =
+    std::is_same_v<V, byte::unsigned_byte>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_unsigned_bytes_v =
+    is_unsigned_byte_v<V> && (is_unsigned_byte_v<Vs> && ...);
+
+template <typename V>
+constexpr static auto is_signed_byte_v =
+    std::is_same_v<V, byte::signed_byte>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_signed_bytes_v =
+    is_signed_byte_v<V> && (is_signed_byte_v<Vs> && ...);
+
+template <typename V>
+constexpr static auto is_byte_v = is_unsigned_byte_v<V> || is_signed_byte_v<V>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_bytes_v =
+    is_byte_v<V> && (is_byte_v<Vs> && ...);
+
+template <typename V>
+constexpr static auto is_class_v = std::is_class_v<V> && !is_byte_v<V>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_classes_v =
+    is_class_v<V> && (is_class_v<Vs> && ...);
 
 template <typename V>
 constexpr static auto is_unsigned_char_v = std::is_same_v<V, unsigned char>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_unsigned_chars_v =
+    is_unsigned_char_v<V> && (is_unsigned_char_v<Vs> && ...);
 
 template <typename V>
 constexpr static auto is_signed_char_v =
     std::is_same_v<V, signed char> || std::is_same_v<V, char>;
 
+template <typename V, typename...Vs>
+constexpr static auto are_signed_chars_v =
+    is_signed_char_v<V> && (is_signed_char_v<Vs> && ...);
+
 template <typename V>
 constexpr static auto is_char_v =
     is_unsigned_char_v<V> || is_signed_char_v<V>;
 
+template <typename V, typename...Vs>
+constexpr static auto are_chars_v =
+    is_char_v<V> && (is_char_v<Vs> && ...);
+
 template <typename V>
 constexpr static auto is_bool_v = std::is_same_v<V, bool>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_bools_v =
+    is_bool_v<V> && (is_bool_v<Vs> && ...);
 
 /**
  * @brief Interpretation of what an integral types is.
@@ -63,19 +106,38 @@ constexpr static auto is_bool_v = std::is_same_v<V, bool>;
  */
 template <typename V>
 constexpr static auto is_integral_v =
-    std::is_integral_v<V> && !is_char_v<V> && !is_bool_v<V>;
+    (std::is_integral_v<V> && !is_char_v<V> && !is_bool_v<V>) || is_byte_v<V>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_integrals_v =
+    is_integral_v<V> && (is_integral_v<Vs> && ...);
 
 template <typename V>
 constexpr static auto is_signed_v =
-    std::is_signed_v<V> && is_integral_v<V>;
+    (std::is_signed_v<V> && is_integral_v<V>) || is_signed_byte_v<V>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_signed_v =
+    is_signed_v<V> && (is_signed_v<Vs> && ...);
 
 template <typename V>
 constexpr static auto is_unsigned_v =
-    std::is_unsigned_v<V> && is_integral_v<V>;
+    (std::is_unsigned_v<V> && is_integral_v<V>) || is_unsigned_byte_v<V>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_unsigned_v =
+    is_unsigned_v<V> && (is_unsigned_v<Vs> && ...);
+
+template <typename V>
+constexpr static auto is_pointer_v = std::is_pointer_v<V>;
+
+template <typename V, typename...Vs>
+constexpr static auto are_pointers_v =
+    is_pointer_v<V> && (is_pointer_v<Vs> && ...);
 
 template <typename V>
 constexpr static auto is_integral_or_pointer_v =
-    is_integral_v<V> || std::is_pointer_v<V>;
+    is_integral_v<V> || is_pointer_v<V>;
 
 template <typename V>
 constexpr static auto is_unsigned_or_pointer_v =
