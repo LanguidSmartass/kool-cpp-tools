323144327a9bbd430c253c992b78e6441830f3b4
LanguidSmartass <jedi.orden@gmail.com>
2019-05-17 05:49:29 +0300

    add licence header

--- tests/src/ascii/ascii.cpp
+++ tests/src/ascii/ascii.cpp
@@ -1,4 +1,4 @@
-/** @file log2.cpp
+/** @file ascii.cpp
  * 
  * @author Novoselov Ivan
  * @email  jedi.orden@gmail.com
@@ -26,27 +26,8 @@
  * 
  */
 
-#include <kcppt/log2.hpp>
+#include <kcppt/ascii.hpp>
 
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-TEST(log2, next_greater_equal) {
-    using kcppt::log2::log2_ge;
-    ASSERT_EQ(log2_ge(0), std::numeric_limits<std::intmax_t>::min());
-    ASSERT_EQ(log2_ge(1), 0);
-    ASSERT_EQ(log2_ge(2), 1);
-    ASSERT_EQ(log2_ge(3), 2);
-    ASSERT_EQ(log2_ge(5), 3);
-    ASSERT_EQ(log2_ge(9), 4);
-}
-
-//TEST(log2, next_less_equal) {
-//    using kcppt::log2::log2_le;
-//    ASSERT_EQ(log2_le(0), std::numeric_limits<std::intmax_t>::min());
-//    ASSERT_EQ(log2_le(1), 0);
-//    ASSERT_EQ(log2_le(2), 1);
-//    ASSERT_EQ(log2_le(3), 2);
-//    ASSERT_EQ(log2_le(5), 2);
-//    ASSERT_EQ(log2_le(9), 3);
-//}
