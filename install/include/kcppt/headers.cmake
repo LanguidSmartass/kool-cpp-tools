32b467cf305d0b6e9f998071e5aaa605dba2f61a
LanguidSmartass <jedi.orden@gmail.com>
2019-05-17 05:29:06 +0300

    change some cmake installation configs

--- install/include/kcppt/headers.cmake
+++ install/include/kcppt/headers.cmake
@@ -20,7 +20,9 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-set(
+list(
+    APPEND
+    
     headers-list-template
     
     allocators/beefy.hpp
